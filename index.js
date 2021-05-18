ef8d1f53fe5a2462e0b3cb58cc4e435b6ab7dee0
sinsang <sw980321@naver.com>
2021-05-18 17:35:56 +0900

    gitignore

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 const express = require("express");
+
+const app = express();
+//var port = normalizePort(process.env.PORT || '3000');
+
+app.get("/", (req, res) => {
+    res.send("Hello World");
+});
+
+app.listen(3000, () => {
+    console.log("돌아가는중")
+});
