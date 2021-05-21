7b194a0301df22466a1d4be5f7d164266e58e2fb
sinsang <sw980321@naver.com>
2021-05-22 00:33:13 +0900

    05.22

--- index.js
+++ index.js
@@ -4,11 +4,49 @@ const key = require("./key");
 const app = express();
 var port = (process.env.PORT || '3000');
 
+function getRandomInt(min, max) {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min)) + min; //최댓값은 제외, 최솟값은 포함
+}
+
 var nowTotal = 0;
 var leftTime = 0;
 var rate = 0;
 var leftTime = 0;
 var nowState = false;
+
+// 좋은 상황에서의 문구
+var dangerComment1_good = [
+    "상황은 더 좋아질 수 있습니다.\n더 노력해주세요.",
+    "우리가 계속 행동한다면, 더 늦출 수 있습니다.",
+    "점점 더 좋아질 겁니다.\n해빙도, 우리도."
+];
+
+// 그저그런 상황에서의 문구
+var dangerComment1_soso = [
+    "멀지 않은 미래, 완전히 해빙이 사라질 수 있습니다",
+    "더 앞당겨진다면, 우리가 기후를 통제할 수 없을겁니다",
+    "나 하나만이 아닙니다, 전 지구인의 운명이 달린 문제입니다."
+];
+
+// 안좋은 상황에서의 문구
+var dangerComment1_bad = [
+    "기후재앙이 코 앞입니다",
+    "언제까지 두고만 볼겁니까?\n해빙이 다 녹을 때 까지?",
+    "탄소감축, 오늘 실천하지 않으면 내일이 사라집니다",
+    ""
+];
+
+// 한반도 부분 문구
+var dangerComment2 = [
+    "대한민국은 천도를 고민해야 할 겁니다.<br/>서울이 침수될 수도 있거든요.",
+    "부산 해운대가 없어질 지도 모릅니다.<br/>부산 앞 바다가 지금도 육지로 올라오고 있습니다.",
+    "미래의 대한민국 제 1의 공항은 어딜까요?<br/>일단 인천공항은 아닙니다. 바다에 잠길 위기입니다.",
+    "대한민국은 2계절이 뚜렷한 나라입니다.<br/>잘못된 것 같다구요?<br/>곧 올바른 말이 될 것입니다.",
+    "이젠 대한민국 겨울에 눈이 내리지 않습니다.<br/>대신 태풍이 칩니다.",
+    "이 지도가 우리 자손에게 생길 일이라 생각하지 마세요.<br/>우리에게도 다가오고 있습니다.",
+];
 
 var apiURL = (year, month) => {
     var startDate = "" + year + month;
@@ -69,8 +107,16 @@ app.use(express.static(__dirname + "/public"));
 
 app.get("/", (req, res) => {
     if (nowState) {
+
+        var dc1 = "";
+        if (rate >= 1.3) dc1 = dangerComment1_bad[getRandomInt(0, dangerComment1_bad.length)];
+        else if (rate >= 1.0) dc1 = dangerComment1_soso[getRandomInt(0, dangerComment1_soso.length)];
+        else dc1 = dangerComment1_good[getRandomInt(0, dangerComment1_good.length)];
+
         res.render("seaIceTimer", {
            rate : rate, 
+           dangerComment1 : dc1,
+           dangerComment2 : dangerComment2[getRandomInt(0, dangerComment2.length)],
            leftYear : leftTime.getFullYear() - 1970,
            leftMonth : leftTime.getMonth(),
            leftDate : leftTime.getDate()
